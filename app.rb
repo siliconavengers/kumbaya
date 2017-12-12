3aec13171f9a9d705b65203ef284b04ab6d956f3
Vincent <vinh.nglx@gmail.com>
2017-12-12 16:49:06 +0800

    return message when finish

--- app.rb
+++ app.rb
@@ -10,6 +10,8 @@ post '/backup' do
   # Execute the command
   command = "PGPASSWORD=#{ENV['PG_PASSWORD']} pg_dump -Fc --no-acl --no-owner -h #{ENV['PG_HOST']} -p #{ENV['PG_PORT']} -U #{ENV['PG_USER_NAME']} #{ENV['PG_DATABASE_NAME']} > asiaboxoffice_#{Time.now}.dump"
   exec(command)
+
+  respond_message "Huray! The backup file is generated!"
 end
 
 post '/backup_from_local' do
