b59c60500084c86478ebbce6a53e51e799e2ad32
Vincent <vincent@siliconjungles.com>
2018-01-19 11:44:33 +0800

    correct auto-delete script

--- auto-delete.sh
+++ auto-delete.sh
@@ -2,6 +2,6 @@
 
 find $PWD/backup/ -type f -name '*.zip' -mtime 1 -exec rm -rf {} \;
 
-find $PWD/postgres/ -type f -name '*.dump' -mtime 1 -exec rm -rf {} \;
+find $PWD/backup/postgres/ -type f -name '*.dump' -mtime 1 -exec rm -rf {} \;
 
-find $PWD/redis/ -type f -name '*.json' -mtime 1 -exec rm -rf {} \;
+find $PWD/backup/redis/ -type f -name '*.json' -mtime 1 -exec rm -rf {} \;
