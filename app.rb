56baef8e82f2e6bfb7a49aa398ad9573cd424414
Vincent <vincent@siliconjungles.com>
2018-01-19 11:09:40 +0800

    add bitly

--- app.rb
+++ app.rb
@@ -5,37 +5,43 @@ require 'aws-sdk-s3'
 require 'mail'
 require 'slack-notifier'
 require 'zip'
+require 'bitly'
 
-configure {
-  set :server, :puma }
+configure { set :server, :puma }
 
 get '/' do
   'Kumbaya! The bot is up!'
 end
 
 post '/auto' do
-  backup
+  now = Time.now
+
+  zip_backup_url = backup(now)
+
+  send_mail(zip_backup_url, now)
 
   notifier = Slack::Notifier.new ENV['WEBHOOK_URL'] do
     defaults channel: ENV['SLACK_CHANNEL'],
               username: ENV['SLACK_USER_NAME']
   end
 
-  notifier.ping "Guys, I sent the email with backup url. Enjoy and good night!"
+  notifier.ping "Morning! I sent an email with backup database. Thanks! Have a nice day!"
 end
 
 post '/backup' do
-  backup
+  now = Time.now
+
+  bitly_link = generate_short_link(backup(now))
 
   notifier = Slack::Notifier.new ENV['WEBHOOK_URL'] do
     defaults channel: ENV['SLACK_CHANNEL'],
               username: ENV['SLACK_USER_NAME']
   end
 
-  notifier.ping "Yoh! I sent the backup files. Check your email pls!"
+  notifier.ping "Done! The backup file for #{now} is here: #{bitly_link}"
 end
 
-def backup
+def backup(now)
   # Execute the command to generate latest backup file
   `PGPASSWORD=$PG_PASSWORD pg_dump -Fc --no-acl --no-owner -h $PG_HOST -p $PG_PORT -U $PG_USER_NAME $PG_DATABASE_NAME > backup/postgres/postgres_$(date +%d-%b-%Y-%H-%M-%S).dump`
 
@@ -45,8 +51,6 @@ def backup
   # Zip backup files
   postgres_file_path = Dir['backup/postgres/*'].sort_by{ |f| File.mtime(f) }.last
   redis_file_path = Dir['backup/redis/*'].sort_by{ |f| File.mtime(f) }.last
-
-  now = Time.now
 
   zip_file_path = "backup/data-backup-#{now}.zip"
 
@@ -60,8 +64,10 @@ def backup
 
   zip_obj = s3.bucket(ENV['AWS_BUCKET']).object(zip_file_path)
   zip_obj.upload_file("#{zip_file_path}")
-  zip_backup_url = zip_obj.presigned_url(:get, expires_in: 60 * 60)
+  zip_obj.presigned_url(:get, expires_in: 60 * 60)
+end
 
+def send_mail(zip_backup_url, now)
   # Sending email - Use DirectMail server of Alicloud for now
   Mail.defaults do
     delivery_method :smtp, {
@@ -74,6 +80,8 @@ def backup
     }
   end
 
+  short_link = generate_short_link(zip_backup_url)
+
   mail = Mail.deliver do
     to      ENV['MAIL_TO']
     from    ENV['MAIL_FROM']
@@ -82,7 +90,7 @@ def backup
       body "
         Yoh!
 
-        Download the backup file at here: #{zip_backup_url}.
+        Download the backup file at here: #{short_link}.
 
         This link only available in 60 minutes.
 
@@ -91,3 +99,9 @@ def backup
   end
 end
 
+def generate_short_link(zip_backup_url)
+  Bitly.use_api_version_3
+  bitly = Bitly.new(ENV['BITLY_USER_NAME'], ENV['BITLY_API_KEY'])
+  bitly_link = bitly.shorten(zip_backup_url)
+  bitly_link.short_url
+end
