0bbf24362ae83e34ce6e99f881138561c860adc4
Vincent <vincent@siliconjungles.com>
2018-01-31 09:57:57 +0800

    delete gpg files

--- auto-delete.sh
+++ auto-delete.sh
@@ -2,6 +2,8 @@
 
 find $PWD/backup/ -type f -name '*.zip' -mtime +1 -exec rm -rf {} \;
 
+find $PWD/backup/ -type f -name '*.gpg' -mtime +1 -exec rm -rf {} \;
+
 find $PWD/backup/postgres/ -type f -name '*.dump' -mtime +1 -exec rm -rf {} \;
 
 find $PWD/backup/redis/ -type f -name '*.json' -mtime +1 -exec rm -rf {} \;
