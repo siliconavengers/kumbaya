42da481016555290cf14d24781a3f9a0d329279b
Vincent <vincent@siliconjungles.com>
2018-03-13 15:06:33 +0800

    delete files from local

--- auto-delete.sh
+++ auto-delete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-find $PWD/backup/ -type f -name '*.zip' -mtime +1 -exec rm -rf {} \;
+find $PWD/backup/ -type f -name '*.zip' -exec rm -rf {} \;
 
-find $PWD/backup/ -type f -name '*.gpg' -mtime +1 -exec rm -rf {} \;
+find $PWD/backup/ -type f -name '*.gpg' -exec rm -rf {} \;
 
-find $PWD/backup/postgres/ -type f -name '*.dump' -mtime +1 -exec rm -rf {} \;
+find $PWD/backup/postgres/ -type f -name '*.dump' -exec rm -rf {} \;
 
-find $PWD/backup/redis/ -type f -name '*.json' -mtime +1 -exec rm -rf {} \;
+find $PWD/backup/redis/ -type f -name '*.json' -exec rm -rf {} \;
